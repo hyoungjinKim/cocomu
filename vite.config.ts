f68b1ea99ffd49f392bbc858e5f57668c0281e58
”isacc7224” <127084974+hyoungjinKim@users.noreply.github.com>
2025-01-27 22:32:46 +0900

    feat(#master): 기본 폴더 구조 설정 및 vite 설정(콘솔 드랍 및 사용하지 않는 모듈 정리, 백 api target 정리)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
@@ -10,7 +9,30 @@ export default defineConfig({
       "^/(api|open)": {
         target: "",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  build: {
+    minify: "esbuild",
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("react")) {
+            return "react-vendor";
+          }
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
       },
     },
   },
